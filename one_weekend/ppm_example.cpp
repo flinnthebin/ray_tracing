c7fefd8c91654076469a20232892ba63074254e7
Chris Flinn <47100787+flinnthebin@users.noreply.github.com>
2024-01-01 17:38:17 +1100

    Add files via upload

---
+++ one_weekend/ppm_example.cpp
@@ -0,0 +1,28 @@
+// RGB image stored in PPM format
+
+#include <iostream>
+#include "vec3.h"
+#include "color.h"
+
+int main()
+{
+	
+	// Image
+	
+	int image_width  = 256;
+	int image_height = 256;
+
+	// Render
+	
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = 0; j < image_height; ++j)
+	{
+		for (int i = 0; i < image_width; ++i)
+		{
+			auto pixel_color = color(double (i) / (image_width - 1), 
+												 double (j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
+		}
+	}
+}
