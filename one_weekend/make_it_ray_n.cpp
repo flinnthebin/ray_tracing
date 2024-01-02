98c3e554b0755d9ab7ecc783b17b0d740636fa57
Chris Flinn <christopher.flinn@gmail.com>
2024-01-03 10:39:05 +1100

    redsphere

--- one_weekend/make_it_ray_n.cpp
+++ one_weekend/make_it_ray_n.cpp
@@ -6,59 +6,80 @@
 
 #include <iostream>
 
+// Graphics trick to linearly scale 0.0 <= a <= 1.0.
+// When a = 0.0, we want white. When a = 1.0, we want blue. In between, we want a blend.
+// This is known as a "linear interpolation" or "lerp" between two values. A lerp is the form:
+// blendedValue = (1 - a) * startValue + a * endValue
+
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant >= 0);
+}
+
+
 color ray_color(const ray& r)
 {
-	return color(0,0,0);
+  if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    return color(1, 0, 0);
+	
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 1.0 * (unit_direction.y() + 1.0);
+  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main()
 {
-	// Image
+  // Image
 
-	auto aspect_ratio = 16.0 / 9.0;
-	int image_width 	= 400;
+  auto aspect_ratio = 16.0 / 9.0;
+  int image_width 	= 400;
 
-	// Calculate the image height and ensure that it is at least 1.
+  // Calculate the image height and ensure that it is at least 1.
 
-	int image_height = static_cast<int>(image_width / aspect_ratio);
-	image_height		 = (image_height < 1) ? 1 : image_height;
+  int image_height = static_cast<int>(image_width / aspect_ratio);
+  image_height = (image_height < 1) ? 1 : image_height;
 
-	// Camera
+  // Camera
 
-	auto focal_length		 = 1.0;
-	auto viewport_height = 2.0;
-	auto viewport_width	 = viewport_height * (static_cast<double>(image_width) / image_height);
-	auto camera_center	 = point3(0,0,0);
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+  auto camera_center = point3(0,0.6,0);
 
-	// Calculate the vectors across the horizontal and down the vertical viewport edges.
+  // Calculate the vectors across the horizontal and down the vertical viewport edges.
+ 
+  auto viewport_u = vec3(viewport_width, 0, 0);
+  auto viewport_v = vec3(0, -viewport_height, 0);
 
-	auto viewport_u = vec3(viewport_width, 0, 0);
-	auto viewport_v = vec3(0, -viewport_height, 0);
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
 
-	// Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
 
-	auto pixel_delta_u = viewport_u / image_width;
-	auto pixel_delta_v = viewport_v / image_height;
+  // Calculate the location of the upper left pixel.
 
-	// Calculate the location of the upper left pixel.
+  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+  auto pixel00_loc				 = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-	auto pixel00_loc				 = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+  // Render
 
-	// Render
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+  for (int j = 0; j < image_height; ++j)
+  {
+    for (int i = 0; i < image_width; ++i)
+    {
+      auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+      auto ray_direction = pixel_center - camera_center;
+      ray r(camera_center, ray_direction);
 
-	for (int j = 0; j < image_height; ++j)
-	{
-		for (int i = 0; i < image_width; ++i)
-		{
-			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-			auto ray_direction = pixel_center - camera_center;
-			ray r(camera_center, ray_direction);
-
-			color pixel_color = ray_color(r);
-			write_color(std::cout, pixel_color);
+      color pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
 		}
 	}
 }
